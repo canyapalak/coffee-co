a17f6ce2c5dc1d9972431003034a42f5f83d2c62
canyapalak <canyapalak@gmail.com>
2023-09-20 15:36:40 +0200

    navbar completed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,8 +9,6 @@ import logo2 from "@/public/assets/images/logo002.png";
 import leaf from "@/public/assets/images/leaf2.png";
 import cartIcon from "@/public/assets/images/cart.png";
 import cartIcon2 from "@/public/assets/images/cart2.png";
-import enIcon from "@/public/assets/images/en01.png";
-import deIcon from "@/public/assets/images/de01.png";
 import { HiMoon, HiSun } from "react-icons/hi";
 
 export default function Navbar() {
@@ -76,174 +74,186 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="mb-24 text-xl">
-        <div className="max-w-screen flex flex-wrap items-center justify-between mx-auto py-2">
-          {isDark ? (
-            <Link href="/">
-              <Image
-                src={logo2}
-                alt="logo"
-                className="h-10 w-40 mb-auto mt-auto"
-              />
-            </Link>
-          ) : (
-            <Link href="/">
-              <Image
-                src={logo}
-                alt="logo"
-                className="h-10 w-40 mb-auto mt-auto"
-              />
-            </Link>
-          )}
-          <div className="language-dropdown w-10 h-8">
-            <div className="selected-language" onClick={toggleDropdown}>
-              <img
-                src={flag}
-                alt={language}
-                className="flag w-6 h-4 mb-2 cursor-pointer ml-[7.5px]"
-              />
-            </div>
-            {isDropdownOpen && (
-              <div className="dropdown-options border-[1px] rounded-lg border-neutral-400 bg-orange-50 p-1">
-                <div
-                  className={`option ${language === "en" ? "selected" : ""}`}
-                  onClick={() => handleLanguageChange("en")}
-                >
-                  <img
-                    src={
-                      "https://res.cloudinary.com/djlyhp6vr/image/upload/v1695206737/en01_bsfg9g.png"
-                    }
-                    alt="en"
-                    className="flag hover:bg-neutral-300 rounded-md p-1 cursor-pointer"
-                  />
-                </div>
-                <div
-                  className={`option ${language === "de" ? "selected" : ""}`}
-                  onClick={() => handleLanguageChange("de")}
-                >
-                  <img
-                    src="https://res.cloudinary.com/djlyhp6vr/image/upload/v1695206732/de01_h5qg1v.png"
-                    alt="de"
-                    className="flag hover:bg-neutral-300 rounded-md p-1 cursor-pointer"
-                  />
-                </div>
-              </div>
+      <div className="mb-24 text-xl max-w-screen flex flex-wrap items-center justify-between mx-auto py-2">
+        {/* LOGO AND LANGUAGE & DARK MODE */}
+        <div className="flex flex-row gap-3 md:gap-7 lg:gap-10 mt-1">
+          <div>
+            {isDark ? (
+              <Link href="/">
+                <Image
+                  src={logo2}
+                  alt="logo"
+                  className="h-10 w-40 mb-auto mt-auto"
+                />
+              </Link>
+            ) : (
+              <Link href="/">
+                <Image
+                  src={logo}
+                  alt="logo"
+                  className="h-10 w-40 mb-auto mt-auto"
+                />
+              </Link>
             )}
           </div>
+          {/* LANGUAGE AND DARK MODE */}
+          <div className="flex flex-row gap-3 mt-2">
+            {/* LANGUAGE */}
+            <div className="language-dropdown w-10 h-8 mt-3 flex-row">
+              <div className="selected-language" onClick={toggleDropdown}>
+                <img
+                  src={flag}
+                  alt={language}
+                  className="flag w-6 h-4 mb-2 cursor-pointer ml-[7.5px]"
+                />
+              </div>
+              {isDropdownOpen && (
+                <div className="dropdown-options border-[1px] rounded-lg border-neutral-400 bg-orange-50 p-1">
+                  <div
+                    className={`option ${language === "en" ? "selected" : ""}`}
+                    onClick={() => handleLanguageChange("en")}
+                  >
+                    <img
+                      src={
+                        "https://res.cloudinary.com/djlyhp6vr/image/upload/v1695206737/en01_bsfg9g.png"
+                      }
+                      alt="en"
+                      className="flag hover:bg-neutral-300 rounded-md p-1 cursor-pointer"
+                    />
+                  </div>
+                  <div
+                    className={`option ${language === "de" ? "selected" : ""}`}
+                    onClick={() => handleLanguageChange("de")}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/djlyhp6vr/image/upload/v1695206732/de01_h5qg1v.png"
+                      alt="de"
+                      className="flag hover:bg-neutral-300 rounded-md p-1 cursor-pointer"
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
+            {/* DARK MODE */}
+            <div className="mt-2">
+              {isDark ? (
+                <HiSun
+                  alt="Sun"
+                  onClick={handleModeClick}
+                  className="w-6 h-6 cursor-pointer mb-2 md:mb-1 dark:text-orange-50"
+                />
+              ) : (
+                <HiMoon
+                  alt="Moon"
+                  onClick={handleModeClick}
+                  className="w-6 h-6 cursor-pointer mb-2 md:mb-1 text-neutral-700"
+                />
+              )}
+            </div>
+          </div>
+        </div>
 
-          <button
-            onClick={toggleMenu}
-            type="button"
-            className="inline-flex items-center p-2 w-12 h-12 justify-center text-sm text-neutral-800 dark:text-neutral-50
+        <button
+          onClick={toggleMenu}
+          type="button"
+          className="inline-flex items-center p-2 w-12 h-12 justify-center text-sm text-neutral-800 dark:text-neutral-50
             rounded-xl md:hidden focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:bg-neutral-50 
             focus:bg-opacity-40 mt-3"
-            aria-controls="navbar-default"
-            aria-expanded={isMenuOpen ? "true" : "false"}
-          >
-            <svg
-              className={` w-5 h-5 ${
-                isMenuOpen ? " text-neutral-800 dark:text-neutral-50" : ""
-              }`}
-              aria-hidden="true"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div
-            ref={menuRef}
-            className={`my-auto w-full md:block md:w-auto ${
-              isMenuOpen ? "block" : "hidden"
+          aria-controls="navbar-default"
+          aria-expanded={isMenuOpen ? "true" : "false"}
+        >
+          <svg
+            className={` w-5 h-5 ${
+              isMenuOpen ? " text-neutral-800 dark:text-neutral-50" : ""
             }`}
-            id="navbar-default"
+            aria-hidden="true"
+            fill="none"
+            viewBox="0 0 17 14"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-3 md:flex-row md:space-x-8 items-center dark:text-orange-50">
-              <div>
-                {isDark ? (
-                  <HiSun
-                    alt="Sun"
-                    onClick={handleModeClick}
-                    className="w-6 h-6 cursor-pointer mb-2 md:mb-1 dark:text-orange-50"
-                  />
-                ) : (
-                  <HiMoon
-                    alt="Moon"
-                    onClick={handleModeClick}
-                    className="w-6 h-6 cursor-pointer mb-2 md:mb-1 text-neutral-700"
-                  />
-                )}
-              </div>
-              <div>
-                <hr className="border-1 border-lime-900 dark:border-lime-200 md:border-0" />
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        {/* MENU TITLES */}
+        <div
+          ref={menuRef}
+          className={`my-auto w-full md:block md:w-auto ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+          id="navbar-default"
+        >
+          <ul
+            className="flex flex-col p-4 md:p-0 mt-3 md:flex-row 
+          lg:space-x-8 space-x-3 items-center dark:text-orange-50"
+          >
+            <div>
+              <hr className="border-1 border-lime-900 dark:border-lime-200 md:border-0" />
+              <Link
+                href="/"
+                className="block py-2 pl-3 pr-4 md:border-0 md:p-0 hover:text-stone-600 dark:hover:text-neutral-300"
+              >
+                {text.home}
+              </Link>
+            </div>
+            <div className="md:border-0 md:p-0 hover:text-stone-600 dark:hover:text-neutral-300">
+              <hr className="border-1 border-lime-900 dark:border-lime-200 md:border-0" />
+              <Link
+                href="/policy"
+                className=" py-2 pl-3 pr-4 hover:text-stone-600 dark:hover:text-neutral-300 flex flex-row "
+              >
+                {text.threeF}
+                <Image src={leaf} alt="Leaf" className="w-6 h-6" />
+              </Link>
+            </div>
+            <div>
+              <hr className="border-1 border-lime-900 dark:border-lime-200 md:border-0" />
+              <Link
+                href="/shop"
+                className="block py-2 pl-3 pr-4 md:border-0 md:p-0 hover:text-stone-600 dark:hover:text-neutral-300"
+              >
+                {text.shop}
+              </Link>
+            </div>
+            <div>
+              <hr className="border-1 border-lime-900 dark:border-lime-200 md:border-0" />
+              {isCartEmpty ? (
                 <Link
-                  href="/"
+                  href="/cart"
                   className="block py-2 pl-3 pr-4 md:border-0 md:p-0 hover:text-stone-600 dark:hover:text-neutral-300"
                 >
-                  {text.home}
+                  {isDark ? (
+                    <Image src={cartIcon2} alt="Cart" className="w-6 h-6" />
+                  ) : (
+                    <Image src={cartIcon} alt="Cart" className="w-6 h-6" />
+                  )}
                 </Link>
-              </div>
-              <div className="md:border-0 md:p-0 hover:text-stone-600 dark:hover:text-neutral-300">
-                <hr className="border-1 border-lime-900 dark:border-lime-200 md:border-0" />
+              ) : (
                 <Link
-                  href="/policy"
-                  className=" py-2 pl-3 pr-4 hover:text-stone-600 dark:hover:text-neutral-300 flex flex-row "
+                  href="/cart"
+                  className="relative block py-2 pl-3 pr-4 md:border-0 md:p-0 hover:text-stone-600 dark:hover:text-neutral-300"
                 >
-                  {text.threeF}
-                  <Image src={leaf} alt="Leaf" className="w-6 h-6" />
-                </Link>
-              </div>
-              <div>
-                <hr className="border-1 border-lime-900 dark:border-lime-200 md:border-0" />
-                <Link
-                  href="/shop"
-                  className="block py-2 pl-3 pr-4 md:border-0 md:p-0 hover:text-stone-600 dark:hover:text-neutral-300"
-                >
-                  {text.shop}
-                </Link>
-              </div>
-              <div>
-                <hr className="border-1 border-lime-900 dark:border-lime-200 md:border-0" />
-                {isCartEmpty ? (
-                  <Link
-                    href="/cart"
-                    className="block py-2 pl-3 pr-4 md:border-0 md:p-0 hover:text-stone-600 dark:hover:text-neutral-300"
-                  >
+                  <div className="relative inline-block">
                     {isDark ? (
                       <Image src={cartIcon2} alt="Cart" className="w-6 h-6" />
                     ) : (
                       <Image src={cartIcon} alt="Cart" className="w-6 h-6" />
                     )}
-                  </Link>
-                ) : (
-                  <Link
-                    href="/cart"
-                    className="relative block py-2 pl-3 pr-4 md:border-0 md:p-0 hover:text-stone-600 dark:hover:text-neutral-300"
-                  >
-                    <div className="relative inline-block">
-                      {isDark ? (
-                        <Image src={cartIcon2} alt="Cart" className="w-6 h-6" />
-                      ) : (
-                        <Image src={cartIcon} alt="Cart" className="w-6 h-6" />
-                      )}
-                      <div
-                        id="cart-circle"
-                        className="absolute top-3 right-2 text-black w-4 h-4 rounded-full flex items-center justify-center text-xs"
-                      >
-                        {cart.length}
-                      </div>
+                    <div
+                      id="cart-circle"
+                      className="absolute top-3 right-2 text-black w-4 h-4 rounded-full flex items-center justify-center text-xs"
+                    >
+                      {cart.length}
                     </div>
-                  </Link>
-                )}
-              </div>
-            </ul>
-          </div>
+                  </div>
+                </Link>
+              )}
+            </div>
+          </ul>
         </div>
       </div>
     </>
