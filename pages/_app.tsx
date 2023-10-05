94a0f3fe141fc89636c601018f3488323e6ccc99
canyapalak <canyapalak@gmail.com>
2023-10-05 23:23:04 +0200

    title updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,7 @@ export default function MyApp({ Component, pageProps }: any) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <title>coffee-co</title>
       </Head>
       <main className={`flex flex-col min-h-screen ${spartan.className}`}>
         <CartProvider>
